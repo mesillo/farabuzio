7c0919aa6569a34668a4c98796a08ba2770ab6ba
Alberto.Mesin <alberto.mesin@fleetmatics.com>
2019-02-11 17:13:33 +0100

    Cose che non ricordo...

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-console.log( process.env );
+//console.log( process.env );
 
 console.log( "PID : " + process.pid );
+setTimeout( () => {
+    throw new Error( " Some stupid error! :-|" );
+}, 3000 );
 
-process.exit( 0 );
+//process.exit( 0 );
