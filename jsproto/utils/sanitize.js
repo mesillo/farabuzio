c33df08c3234debc4cc291c041db353fbec402b3
Alberto.Mesin <alberto.mesin@fleetmatics.com>
2019-02-14 11:28:28 +0100

    XSSsanitizeString

--- jsproto/utils/sanitize.js
+++ jsproto/utils/sanitize.js
@@ -3,6 +3,11 @@
 let objectToSanitize = {"results":[{"address_components":[{"long_name":"277","short_name":"277","types":["street_number"]},{"long_name":"Bedford Avenue","short_name":"Bedford Ave","types":["route"]},{"long_name":"Williamsburg","short_name":"Williamsburg","types":["neighborhood","political"]},{"long_name":"Brooklyn","short_name":"Brooklyn","types":["sublocality","political"]},{"long_name":"Kings","short_name":"Kings","types":["administrative_area_level_2","political"]},{"long_name":"New York","short_name":"NY","types":["administrative_area_level_1","political"]},{"long_name":"United States","short_name":"US","types":["country","political"]},{"long_name":"11211","short_name":"11211","types":["postal_code"]}],"formatted_address":"277 Bedford Avenue, Brooklyn, NY 11211, USA","geometry":{"location":{"lat":40.714232,"lng":-73.9612889},"location_type":"ROOFTOP","viewport":{"northeast":{"lat":40.7155809802915,"lng":-73.9599399197085},"southwest":{"lat":40.7128830197085,"lng":-73.96263788029151}}},"place_id":"ChIJd8BlQ2BZwokRAFUEcm_qrcA","types":["street_address"]}],"status":"OK"};
 
 //console.dir( objectToSanitize, { showHidden: false, depth: null, colors: true } );
+const sanitizeDefines = Object.freeze( {
+	toReplace: {
+		" ": "_",
+	}
+});
 
 class ObjectUtils {
 	static doForAll( object, targetType, callback ) {
@@ -15,9 +20,9 @@ class ObjectUtils {
 			for( let key of keys ) {
 				object[ key ] = ObjectUtils.doForAll( object[ key ], targetType, callback );
 			}
-		} else {
+		}/* else {
 			console.warn( " === >  Excluded type: " + type );
-		}
+		} */
 
 		return object;
 	}
@@ -25,7 +30,17 @@ class ObjectUtils {
 	static doForAllStrings(  object, callback ) {
 		return ObjectUtils.doForAll(  object, "string", callback );
 	}
+
+	static XSSsanitizeString( unsafeString ) {
+		return unsafeString.replace( /\W/g, ( match ) => {
+			console.log( match + " ==> " + ( sanitizeDefines.toReplace[ match ] || match ) );
+			return ( sanitizeDefines.toReplace[ match ] || match );
+		} );
+	}
 }
+
+ObjectUtils.doForAllStrings( objectToSanitize, ObjectUtils.XSSsanitizeString );
+console.dir( objectToSanitize, { showHidden: false, depth: null, colors: true } );
 
 //let testCallBack = ( string ) => {
 //	//console.log( string );
