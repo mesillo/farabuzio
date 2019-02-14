91d6795b01642087c5d6b3fc6e58d2aa95042838
Alberto.Mesin <alberto.mesin@fleetmatics.com>
2019-02-14 11:57:15 +0100

    More or less definitive.

--- jsproto/utils/sanitize.js
+++ jsproto/utils/sanitize.js
@@ -1,6 +1,100 @@
 #!/usr/bin/env node
 
-let objectToSanitize = {"results":[{"address_components":[{"long_name":"277","short_name":"277","types":["street_number"]},{"long_name":"Bedford Avenue","short_name":"Bedford Ave","types":["route"]},{"long_name":"Williamsburg","short_name":"Williamsburg","types":["neighborhood","political"]},{"long_name":"Brooklyn","short_name":"Brooklyn","types":["sublocality","political"]},{"long_name":"Kings","short_name":"Kings","types":["administrative_area_level_2","political"]},{"long_name":"New York","short_name":"NY","types":["administrative_area_level_1","political"]},{"long_name":"United States","short_name":"US","types":["country","political"]},{"long_name":"11211","short_name":"11211","types":["postal_code"]}],"formatted_address":"277 Bedford Avenue, Brooklyn, NY 11211, USA","geometry":{"location":{"lat":40.714232,"lng":-73.9612889},"location_type":"ROOFTOP","viewport":{"northeast":{"lat":40.7155809802915,"lng":-73.9599399197085},"southwest":{"lat":40.7128830197085,"lng":-73.96263788029151}}},"place_id":"ChIJd8BlQ2BZwokRAFUEcm_qrcA","types":["street_address"]}],"status":"OK"};
+let objectToSanitize = {
+	"results": [
+	  {
+		"address_components": [
+		  {
+			"long_name": "277",
+			"short_name": "277",
+			"types": [
+			  "street_number"
+			]
+		  },
+		  {
+			"long_name": "Bedford Avenue",
+			"short_name": "Bedford Ave",
+			"types": [
+			  "route"
+			]
+		  },
+		  {
+			"long_name": "Williamsburg",
+			"short_name": "Williamsburg",
+			"types": [
+			  "neighborhood",
+			  "political"
+			]
+		  },
+		  {
+			"long_name": "Brooklyn",
+			"short_name": "Brooklyn",
+			"types": [
+			  "sublocality",
+			  "political"
+			]
+		  },
+		  {
+			"long_name": "Kings",
+			"short_name": "Kings",
+			"types": [
+			  "administrative_area_level_2",
+			  "political"
+			]
+		  },
+		  {
+			"long_name": "New York",
+			"short_name": "NY",
+			"types": [
+			  "administrative_area_level_1",
+			  "political"
+			]
+		  },
+		  {
+			"long_name": "United States",
+			"short_name": "US",
+			"types": [
+			  "country",
+			  "political"
+			]
+		  },
+		  {
+			"long_name": "11211",
+			"short_name": "11211",
+			"types": [
+			  "postal_code"
+			]
+		  }
+		],
+		"formatted_address": "277 Bedford Avenue, Brooklyn, NY 11211, USA",
+		"geometry": {
+		  "location": {
+			"lat": 40.714232,
+			"lng": -73.9612889
+		  },
+		  "location_type": "ROOFTOP",
+		  "viewport": {
+			"northeast": {
+			  "lat": 40.7155809802915,
+			  "lng": -73.9599399197085
+			},
+			"southwest": {
+			  "lat": 40.7128830197085,
+			  "lng": -73.96263788029151
+			}
+		  }
+		},
+		"place_id": "ChIJd8BlQ2BZwokRAFUEcm_qrcA",
+		"types": [
+		  "street_address"
+		]
+	  },
+	  {
+		  "BadString": "<script>document.getElementById(\"demo\").innerHTML = \"Hello JavaScript!\";</script>",
+	  }
+	],
+	"status": "OK"
+  };
 
 //console.dir( objectToSanitize, { showHidden: false, depth: null, colors: true } );
 const sanitizeDefines = Object.freeze( {
@@ -12,12 +106,12 @@ const sanitizeDefines = Object.freeze( {
 		"\"": "&quot",
 		"'": "&#39",
 		// Destructive substitutions
-		"(": "",
-		")": "",
-		"[": "",
-		"]": "",
-		"{": "",
-		"}": ""
+		"(": " ",
+		")": " ",
+		"[": " ",
+		"]": " ",
+		"{": " ",
+		"}": " "
 	}
 });
 
@@ -45,7 +139,7 @@ class ObjectUtils {
 
 	static XSSsanitizeString( unsafeString ) {
 		return unsafeString.replace( /\W/g, ( match ) => {
-			console.log( match + " ==> " + ( sanitizeDefines.toReplace[ match ] || match ) );
+			//console.log( match + " ==> " + ( sanitizeDefines.toReplace[ match ] || match ) );
 			return ( sanitizeDefines.toReplace[ match ] || match );
 		} );
 	}
