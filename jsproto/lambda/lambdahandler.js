9050adbe82cf67ede76c82ed75b683a40d134de4
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2019-05-15 11:09:48 +0200

    support decompression.

--- jsproto/lambda/lambdahandler.js
+++ jsproto/lambda/lambdahandler.js
@@ -1,12 +1,20 @@
 "use strict"
 
+let unzipSync = require( "zlib" ).unzipSync;
+
 console.log('Loading function');
 
 exports.handler = async function(event, context) {
     //console.log(JSON.stringify(event, null, 2));
     event.Records.forEach(function(record) {
         // Kinesis data is base64 encoded so decode here
-        var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+        let payload = new Buffer(record.kinesis.data, 'base64');//.toString('ascii');
+        try{
+            payload = unzipSync( payload );
+        } catch( error ) {
+            console.info( "Not compressed payload." );
+        }
+        payload = payload.toString();
         console.log('Decoded payload:', payload);
         console.log( "Decoded payload type:", typeof payload );
     });
