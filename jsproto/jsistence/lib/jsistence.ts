f82a1c602c676e7fd0347309050b23dc247dbb75
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-09-10 12:17:38 +0200

    Stocazzo

--- jsproto/jsistence/lib/jsistence.ts
+++ jsproto/jsistence/lib/jsistence.ts
@@ -1,21 +1,19 @@
-"use strict"
-
 import { EventEmitter } from "events";
 import * as fs from "fs";
 
 class JSistence extends EventEmitter {
-    private filePath : string = null;
+    private filePath : string | null = null;
     private data : Map<any, any>;
 
-    constructor( filePath ) {
+    constructor( filePath : string ) {
         super();
 
         if( typeof filePath !== "string" ) {
             throw new Error( "Wrong parameter type; constructor need a string." );
-        }
+		}
+		this.filePath = filePath;		
 
 		if( fs.existsSync( filePath ) ) { // file already exists...
-			this.filePath = filePath;
 			this.read();
 		} else { // file don't exists
 			this.data = new Map();
@@ -25,8 +23,11 @@ class JSistence extends EventEmitter {
 	}
 
 	private read() : void {
-
-	}	
+		if( this.filePath ) {
+			let inputBuffer : Buffer = fs.readFileSync( this.filePath );
+			let inputString : string = inputBuffer.toString();
+		}
+	}
 
 	private write() : void {
 
