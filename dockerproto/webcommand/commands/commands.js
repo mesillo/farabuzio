f472e00d9772f0f2e28630c6b27a0e52b215f033
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-09-26 17:23:15 +0200

    Porco mondolo.

--- dockerproto/webcommand/commands/commands.js
+++ dockerproto/webcommand/commands/commands.js
@@ -4,11 +4,31 @@ const COMMANDS_DEFINITIONS = {
 	"checkUser": {
 		cmd: "whoami",
 		parameters: []
+	},
+	"uname": {
+		cmd: "uname -a",
+		parameters: []
 	}
 };
 
-let getCommands = () => {
-	return COMMANDS_DEFINITIONS;
-};
+class Commands {
+	/**
+	 * return the configuration json
+	 * @static
+	 * @returns {any}
+	 */
+	static getCommands() {
+		return COMMANDS_DEFINITIONS;
+	}
 
-module.exports = getCommands;
+	/**
+	 * return the available commands
+	 * @static
+	 * @returns {string[]}
+	 */
+	static getCommandsìList() {
+		return Object.keys( Commands.getCommands() );
+	}
+}
+
+module.exports = Commands;
--- dockerproto/webcommand/index.js
+++ dockerproto/webcommand/index.js
@@ -1,26 +1,13 @@
 "use strict";
 
 const http = require( "http" );
-const child_process = require( "child_process" );
 const url = require( "url" );
-const commands = require( "./commands/commands" );
+const Commands = require( "./commands/commands" );
+const Render = require( "./render/render" );
+const Executor = require( "./executor/executor" );
 
 const PORT = 8181;
 const COMMANDURL = "/command";
-
-const execute = ( command ) => {
-	return new Promise( ( resolve, reject ) => {
-		child_process.exec( command, ( error, stdour, stderr ) => {
-			if( error ) {
-				reject( error );
-			}
-			resolve( {
-				stdout: stdour,
-				stderr: stderr
-			} );
-		} );
-	} );
-};
 
 let checkCommandRequest = ( request ) => {
 	let parsedRequest = url.parse( request.url, true );
@@ -31,24 +18,24 @@ let checkCommandRequest = ( request ) => {
 
 let manageRequest = ( request , response ) => {
 	checkCommandRequest( request );
-	response.write( request.url ); //write a response to the client
+	//response.write( request.url ); //write a response to the client
+	Render.setHeaders( response );
+	Render.drawCommandForm( response );
+	Render.drawSeparator( response );
+
 	response.end(); //end the response
 };
 
 let server = http.createServer( manageRequest );
 
-execute( "whoami" )
+Executor.execute( "whoami" )
 	.then( ( streams ) => {
-		//console.dir(
-		//	streams,
-		//	{ depth : null }
-		//);
 		if( streams.stdour === "root\n" ) {
 			console.error( "Not as Root!!!" );
 			process.exit( 255 );
 		}
 		server.listen( PORT ); //the server object listens on port PORT
-		console.log( "WebComman listening on port " + PORT );
+		console.log( "WebCommand listening on port " + PORT );
 	} )
 	.catch( ( error ) => {
 		console.error( error );
