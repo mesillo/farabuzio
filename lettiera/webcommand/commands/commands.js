12362508d458e570fe620f674b0fe927264e4b22
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-09-27 17:32:38 +0200

    last modifications pre vacation.

--- lettiera/webcommand/commands/commands.js
+++ lettiera/webcommand/commands/commands.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const COMMANDS_DEFINITIONS = {
+	"create_kinesis_stream": {
+		cmd: "awslocal kinesis create-stream --stream-name %streamName% --shard-count %shardCount%",
+		parameters: [ "streamName", "shardCount" ]
+	},
 	"checkUser": {
 		cmd: "whoami",
 		parameters: []
@@ -12,9 +16,17 @@ const COMMANDS_DEFINITIONS = {
 		cmd: "ls -lah %dirname%",
 		parameters: [ "dirname" ]
 	},
-	"create_kinesis_stream": {
-		cmd: "awslocal kinesis create-stream --stream-name %streamName% --shard-count %shardCount%",
-		parameters: [ "streamName", "shardCount" ]
+	"touch_test": {
+		cmd: "touch storage/testfile",
+		parameters: []
+	},
+	"edit_test": {
+		cmd: "echo FILE_CONTENT >> storage/testfile",
+		parameters: []
+	},
+	"cat_test": {
+		cmd: "cat storage/testfile",
+		parameters: []
 	}
 };
 
