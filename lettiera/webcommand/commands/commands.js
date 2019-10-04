0f74ef58936578c62561e70ed3844a41b60a6371
Alberto Mesin <a.mesin@libero.it>
2019-10-04 23:06:40 +0200

    Added command for kinesis-lambda sandbox.

--- lettiera/webcommand/commands/commands.js
+++ lettiera/webcommand/commands/commands.js
@@ -1,11 +1,21 @@
 "use strict";
 
+const defines = require( "../defines" );
+
 const COMMANDS_DEFINITIONS = {
 	"create_kinesis_stream": {
 		cmd: "awslocal kinesis create-stream --stream-name %streamName% --shard-count %shardCount%",
 		parameters: [ "streamName", "shardCount" ]
 	},
-	"checkUser": {
+	"create_lambda_function": {
+		cmd: `awslocal lambda create-function --function-name=%functionName% --runtime=nodejs8.10  --region=fakeRegion --role=fakeRole --handler=%handlerFile%.%handlerFunction% --zip-file fileb://${defines.STORAGEPATH}%zipFileName%`, //TODO: review Storage and runtime...
+		parameters: [ "functionName", "handlerFile", "handlerFunction", "zipFileName" ]
+	},
+	"create_event_mapping": {
+		cmd: "awslocal lambda create-event-source-mapping --function-name %functionName% --event-source arn:aws:kinesis:us-east-1:000000000000:stream/%streamName% --batch-size %batchSize% --starting-position TRIM_HORIZON --region=fakeRegion", //TODO: implement --starting-position
+		parameters: [ "functionName", "streamName", "batchSize" ]
+	}
+	/*,	"checkUser": {
 		cmd: "whoami",
 		parameters: []
 	},
@@ -27,7 +37,7 @@ const COMMANDS_DEFINITIONS = {
 	"cat_test": {
 		cmd: "cat storage/testfile",
 		parameters: []
-	}
+	}*/
 };
 
 class Commands {
