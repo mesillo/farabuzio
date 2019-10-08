f587d19e833c06ee6409162c55070a7684dddf50
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-10-08 17:35:09 +0200

    More style and comments.

--- lettiera/start-services.sh
+++ lettiera/start-services.sh
@@ -3,8 +3,6 @@
 echo "=== starting sandbox ==="
 # starting webcommand application
 su - localstack -c 'cd /opt/code/localstack/webcommand/ && npm start' &
-# runuser -l  localstack -c 'node webcommand/index.js' &
-# node webcommand/index.js &
 
 echo "=== starting localstack services ==="
 # starting localstack
