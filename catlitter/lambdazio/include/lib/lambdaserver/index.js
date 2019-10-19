2e02a48a4f7039e5099445f511751a0a0dc9e680
Alberto Mesillo Mesin <mesin.alberto@gmail.com>
2019-10-19 12:14:59 +0200

    cose non del tutto cosate

--- catlitter/lambdazio/include/lib/lambdaserver/index.js
+++ catlitter/lambdazio/include/lib/lambdaserver/index.js
@@ -1,7 +1,24 @@
 "use strict";
 
-class LambdaServer {
-    constructor( lambdaName, port )
+const fileSystemDir = "../../../fs/";
+
+const defaultConfigurations = {
+    lambdaName : "lambdaTest",
+    lambdaFile : "main",
+    lambdaHandler : "handler",
+    httpPort : 9000,
+    processTimeToLive : null //Not used for the moment...
 }
 
-module.export = LambdaServer;
+class LambdaServer {
+    constructor( configuration = defaultConfigurations ) {
+        // TODO: checks and Exceptions management...
+        this.handler = require( fileSystemDir + configuration.lambdaName + "/" + configuration.lambdaFile )[ configuration.lambdaHandler ];
+    }
+
+    /* fire( event, contest ) {
+        this.handler( event, contest );
+    } */
+}
+
+module.exports = LambdaServer;
