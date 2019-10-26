a26ceb629fe1a99d601852907a92b88cb10ec49e
Alberto Mesin <a.mesin@libero.it>
2019-10-26 22:29:54 +0200

    subprocess.check_output

--- pyproto/lambda/handler.py
+++ pyproto/lambda/handler.py
@@ -1,15 +1,18 @@
-import os
+#import os
+import subprocess
 
 LAMBDA_NAME = "stocazzo"
 
 def handler( event, context ):
-    print( event )
-    print( context )
-    #print( LAMBDA_NAME )
-    os.system( 'curl google.com' )
+	print( event )
+	print( context )
+	#print( LAMBDA_NAME )
+	#os.system( 'curl google.com' )
+	returned_output = subprocess.check_output( [ "curl", "google.com" ] )
+	print( returned_output.decode( "utf-8" ) )
 
-    return { 
-        'message' : 'Lambda End.'
-    }
+	return { 
+		'message' : 'Lambda End.'
+	}
 
 handler( 'event', 'context' ) #TODO: remove
