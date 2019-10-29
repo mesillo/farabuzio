369380ba441bfd289a88a784ac952fe6e16f1a7b
Alberto Mesillo Mesin <mesin.alberto@gmail.com>
2019-10-29 22:46:58 +0100

    Scorese a raffica

--- catlitter/lambdazio/app.js
+++ catlitter/lambdazio/app.js
@@ -2,21 +2,29 @@
 
 const http = require( "http" );
 
+const lambdaServer = require( "./include/lib/lambdaserver" );
+
 let port = 9999;
 let lambdaName = "lambdaTest";
 //let lambdaFile = "main";
 let lambdaFile = "singletontest";
 let lambdaHandler = "handler";
 
-let handler = require( `./fs/${lambdaName}/${lambdaFile}` )[ lambdaHandler ];
+//let handler = require( `./fs/${lambdaName}/${lambdaFile}` )[ lambdaHandler ];
+
+let lambdaSvr = new lambdaServer( "./fs/" );
 
 let requestMnrg = async ( request, response ) => {
-    //console.dir(
-    //    request,
-    //    { depth : null }
-    //);
-    await handler( "event", "context" );
-    response.end();
+	/*console.dir(
+		request.url,
+		{ depth : null }
+	);*/
+	//await handler( "event", "context" );
+	if( request.url === "/reset" ) {
+		lambdaSvr = new lambdaServer( "./fs/" );
+	}
+	let lresult = await lambdaSvr.fireLambda( "lambdaTest", "event", "context" );
+	response.end( lresult );
 }
 
 let server = http.createServer( requestMnrg );
--- catlitter/lambdazio/include/lib/lambdaserver/index.js
+++ catlitter/lambdazio/include/lib/lambdaserver/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const fileSystemDir = "../../../fs/";
-const fs = require( "fs" ).promises;
+const fs = require( "fs" );
+const path = require( "path" );
 
 const defaultConfigurations = {
 	lambdaName : "lambdaTest",
@@ -17,9 +18,47 @@ class LambdaServer {
 		this.handler = require( fileSystemDir + configuration.lambdaName + "/" + configuration.lambdaFile )[ configuration.lambdaHandler ];
 	}*/
 	constructor( lambdaStorage = null ) {
-		if( ! lambdaStorage ) {
-			fs.lstat( lambdaStorage )
+		this.lambdaHandlers = [];
+		if( lambdaStorage && fs.lstatSync( lambdaStorage ).isDirectory() ) {
+			this._scanDirectory( lambdaStorage );
 		}
+		console.dir( this.lambdaHandlers );
+	}
+
+	_scanDirectory( lambdaStorage ) {
+		let files = fs.readdirSync( lambdaStorage );
+		for( let file of files ) {
+			let testPath = `${lambdaStorage}/${file}`;
+			if( fs.lstatSync( testPath ).isDirectory() ) {
+				this._tryToDesribeLambda( testPath );
+			}
+		}
+	}
+
+	_tryToDesribeLambda( testPath ) {
+		let files = fs.readdirSync( testPath );
+		for( let file of files ) {
+			if( file === "lambda.json" ) {
+				//let configData = JSON.parse( fs.readFileSync( testPath + "/lambda.json" ) );
+				let relativePath = path.relative( __dirname, testPath );
+				let configData = require( relativePath + "/lambda.json" );
+				this._addConfiguration( relativePath, configData );
+			}
+		}
+	}
+
+	_addConfiguration( relativePath, configData ) {
+		// TODO: log informations.
+		this.lambdaHandlers[ configData.lambdaName ] = require( relativePath +"/"+ configData.lambdaFile )[ configData.lambdaHandler ];
+	}
+
+	async fireLambda( lambdaName, event, context ) {
+		if( this.lambdaHandlers[ lambdaName ] ) {
+			// TODO: checks on event and context.. ??
+			// TODO: use call() or other method to change the context of call???
+			return await this.lambdaHandlers[ lambdaName ]( event, context );
+		}// else TODO: log something???
+		return null;
 	}
 }
 
