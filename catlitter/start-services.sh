b56a4393c9953d10f82b7e41c3aa40c6e9c9807d
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-10-30 16:43:12 +0100

    pippo e paperino

--- catlitter/start-services.sh
+++ catlitter/start-services.sh
@@ -4,6 +4,10 @@ echo "=== starting sandbox ==="
 # starting webcommand application
 su - localstack -c 'cd /opt/code/localstack/fbzwebcommand/ && npm start' &
 
+echo "=== starting node lambda server ==="
+# starting node lambda server
+su - localstack -c 'cd /opt/code/localstack/lambdazio/ && npm start' &
+
 echo "=== starting localstack services ==="
 # starting localstack
 docker-entrypoint.sh
