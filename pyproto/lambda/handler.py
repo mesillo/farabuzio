b48017f49d4e7d1ecc1aab88d5411caee1ec6e67
Alberto Mesin <alberto.mesin@fleetmatics.com>
2019-10-30 16:21:18 +0100

    Cose su python

--- pyproto/lambda/handler.py
+++ pyproto/lambda/handler.py
@@ -2,13 +2,21 @@
 import subprocess
 
 LAMBDA_NAME = "stocazzo"
+LAMBDA_PORT = "9999"
 
 def handler( event, context ):
 	print( event )
 	print( context )
 	#print( LAMBDA_NAME )
 	#os.system( 'curl google.com' )
-	returned_output = subprocess.check_output( [ "curl", "google.com" ] )
+	returned_output = subprocess.check_output( [
+		"curl",
+		"-d",
+		"",
+		"-X",
+		"POST",
+		"localhost:" + LAMBDA_PORT
+	] )
 	print( returned_output.decode( "utf-8" ) )
 
 	return { 
