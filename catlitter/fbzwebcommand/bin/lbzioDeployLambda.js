fc6083950bc15a8d1d95c48af16690a8e5557ea2
Alberto Mesillo Mesin <mesin.alberto@gmail.com>
2019-11-05 23:15:20 +0100

    cose che non ho potuto testare

--- catlitter/fbzwebcommand/bin/lbzioDeployLambda.js
+++ catlitter/fbzwebcommand/bin/lbzioDeployLambda.js
@@ -10,18 +10,33 @@ let configurations = {
 
 ///// Functions /////
 let createTempFile = ( config ) => {
-	let filename = config.tempDirectory +"/"+ ( Math.abs( Math.round( Math.random() * 1000000 ) ) ) + ".py";
+	let handlerName = "LH" + ( Math.abs( Math.round( Math.random() * 1000000 ) ) );
+	//let filename = config.tempDirectory +"/"+ handlerName + ".py";
+	//let filename = config.tempDirectory +"/"+ handlerName + ".py";
+	let filename = handlerName + ".py";
+	//let zipFilename = config.tempDirectory +"/"+ handlerName + ".zip";
+	let zipFilename = handlerName + ".zip";
 	let template = __dirname + "/lib/templates/handler.py";
-	let replaceCommand = `cat ${template} | sed 's/=lambda=name=placeholder=/${config.functionName}/g' > ${filename}`;
-	let deployLambdaCommand = `awslocal lambda create-function --function-name=${config.functionName} --runtime=python2.7 --region=fakeRegion --role=fakeRole --handler=${filename}.hendler file://${filename}`; //TODO: check the runtime (python3???)...
+	let replaceCommand = `cat ${template} | sed 's/=lambda=name=placeholder=/${config.functionName}/g' > ${config.tempDirectory}/${filename}`;
+	let zipCommand = `cd ${config.tempDirectory} && zip -q ${zipFilename} ${filename} && rm ${filename} && cd -`;
+	let deployLambdaCommand = `awslocal lambda create-function --function-name=${config.functionName} --handler=${handlerName}.hendler --runtime=python2.7 --region=fakeRegion --role=fakeRole --zip-file fileb://${config.tempDirectory}/${zipFilename}`; //TODO: check the runtime (python3???)...
+	//console.log( deployLambdaCommand );
 	//let removeCommand = `rm ${filename}`; //TODO: enable after tests...
 	Executor.execute( replaceCommand )
 		.then( async ( streams ) => {
 			console.log( `Created handler for ${config.functionName} function in ${filename}...` );
-			Executor.execute( deployLambdaCommand )
+			Executor.execute( zipCommand )
 				.then( async ( streams ) => {
-					console.log( `Lambda Function ${config.functionName} deployed!` );
-					//TODO: remove lambda handler after deploy...
+					console.log( `Zipped py file in ${zipFilename}` );
+					Executor.execute( deployLambdaCommand )
+						.then( ( streams ) => {
+							console.log( `Lambda Function ${config.functionName} deployed!` );
+							//TODO: remove lambda handler after deploy...
+						} )
+						.catch( ( error ) => {
+							console.error( error );
+							process.exit( 255 ); //TODO: redefine...
+						} );
 				} )
 				.catch( ( error ) => {
 					console.error( error );
