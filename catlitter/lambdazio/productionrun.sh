bc712cf1af9f279e85035ec5a470be4dfb97b4d9
Alberto Mesin <alberto.mesin@visirun.com>
2019-11-11 11:29:28 +0100

    Logs

--- catlitter/lambdazio/productionrun.sh
+++ catlitter/lambdazio/productionrun.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+OUTPUTDIR="../storage/"
+LOGFILENAME=$OUTPUTDIR$(date +%Y%m%d-%H%M)_nodelambda.log;
+
+echo $LOGFILENAME
+
 while true; do
 	echo "Starting lambdazio..."
-	npm start
+	echo "... log writed also on :"$LOGFILENAME"..."
+	npm start | tee $LOGFILENAME
 	echo "... exited."
 done
