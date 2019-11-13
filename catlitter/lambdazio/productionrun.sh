82f8b4f437572cf1604a8f81e29068a707915f92
Alberto Mesin <alberto.mesin@visirun.com>
2019-11-13 11:32:21 +0100

    Change log name on reboot... untested

--- catlitter/lambdazio/productionrun.sh
+++ catlitter/lambdazio/productionrun.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 OUTPUTDIR="../storage/"
-LOGFILENAME=$OUTPUTDIR$(date +%Y%m%d-%H%M)_nodelambda.log;
-
-echo $LOGFILENAME
 
 while true; do
+	LOGFILENAME=$OUTPUTDIR$(date +%Y%m%d-%H%M)_nodelambda.log;
 	echo "Starting lambdazio..."
 	echo "... log writed also on :"$LOGFILENAME"..."
 	npm start | tee $LOGFILENAME
