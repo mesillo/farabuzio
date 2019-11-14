000196de4d633cb76a0b56ff4c07de4ebd2b54e1
Alberto Mesillo Mesin <mesin.alberto@gmail.com>
2019-11-14 01:24:24 +0100

    test... cose

--- catlitter/testsAndMocks/lambdazio/stressLambdazio.js
+++ catlitter/testsAndMocks/lambdazio/stressLambdazio.js
@@ -1,7 +1,13 @@
 #! /usr/bin/env node
 "use strict";
 
-const http = require( "https" );
+const http = require( "http" );
+
+let lambdaNames = [ "banner", "echo", "exception" ];
+let requestInterval = 100;
+let stopTimeout = 10000;
+
+let requestCounter = 0;
 
 const baseObject = {
 	lambda : "",
@@ -18,25 +24,43 @@ const options = {
 	method: "POST",
 	headers: {
 		"Content-Type": "application/json",
-		"Content-Length": requestData.length
+		//"Content-Length": requestData.length //TODO: setup content-length
 	}
 };
 
 const print = ( data ) => {
-	process.stdout.write( data ); //TODO; console.log() ???????
+	//process.stdout.write( data ); //TODO; console.log() ???????
+	console.log( data.toString() );
 };
 
-const request = http.request( options, ( response ) => {
-	console.log( `statusCode: ${response.statusCode}` );
+const makeRequets = ( requestData ) => {
+	const request = http.request( options, ( response ) => {
+		console.log( `statusCode: ${response.statusCode}` );
 
-	response.on( "data", ( data ) => {
-		print( data );
+		response.on( "data", ( data ) => {
+			print( data );
+		} );
+	})
+
+	request.on( "error", ( error ) => {
+		console.error( error );
 	} );
-})
 
-request.on( "error", ( error ) => {
-	console.error( error );
-} );
+	request.write( requestData );
+	request.end();
+};
 
-request.write( requestData );
-request.end();
+let index = 0;
+
+let testTrigger = setInterval( () => {
+	//console.log( lambdaNames[ index ] );
+	baseObject.lambda = lambdaNames[ index ];
+	index = (index + 1) % lambdaNames.length;
+	let requestData = JSON.stringify( baseObject );
+	requestCounter++;
+	makeRequets( requestData );
+}, requestInterval );
+
+setTimeout( () => {
+	clearInterval( testTrigger );
+}, stopTimeout );
