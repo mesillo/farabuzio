3a9a5fb21a7d9c03b70b9c8eb8ab64a0ebca2861
Alberto Mesin <a.mesin@libero.it>
2019-12-02 22:21:05 +0100

    stocazzo

--- jsproto/kinesalite/index.js
+++ jsproto/kinesalite/index.js
@@ -1,30 +1,36 @@
 "use strict";
 
-const KinesaliteClient = require( "./kinesaliteClient" );
+//const KinesaliteClient = require( "./kinesaliteClient" );
+const KinesaliteStreamClient = require( "./kinesaliteStreamClient" );
 
-let kinesis = new KinesaliteClient();
+//let kinesis = new KinesaliteClient();
 let streamName = "testStream";
 let shardNum = 2;
 
-kinesis.createStream( streamName, shardNum )
-	.then( async ( createResult ) => {
-		/*kinesis.waitForStream( streamName )
-			.then( () => {
-			} );*/
-		await kinesis.waitForStream( streamName );
-		/*console.dir(
-			await kinesis.listConsumers( streamName ),
-			{ depth : null }
-		);*/
-	} );
+let stream = new KinesaliteStreamClient( streamName, shardNum );
 
-kinesis.listStreams()
-		.then( ( strList ) => {
-			console.dir( strList );
-			/*for( let streamName of strList ) {
-				kinesis.deleteStream( streamName );
-			}*/
-		} );
-
-kinesis.streamExists( streamName )
+stream.describe()
 	.then( console.dir );
+
+//kinesis.createStream( streamName, shardNum )
+//	.then( async ( createResult ) => {
+//		/*kinesis.waitForStream( streamName )
+//			.then( () => {
+//			} );*/
+//		await kinesis.waitForStream( streamName );
+//		/*console.dir(
+//			await kinesis.listConsumers( streamName ),
+//			{ depth : null }
+//		);*/
+//	} );
+//
+//kinesis.listStreams()
+//		.then( ( strList ) => {
+//			console.dir( strList );
+//			/*for( let streamName of strList ) {
+//				kinesis.deleteStream( streamName );
+//			}*/
+//		} );
+//
+//kinesis.streamExists( streamName )
+//	.then( console.dir );
