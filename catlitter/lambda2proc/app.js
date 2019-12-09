646eff59e404115bd2999dce3c7168484d37df42
Alberto Mesin <alberto.mesin@visirun.com>
2019-12-09 17:47:56 +0100

    Batch transformer start

--- catlitter/lambda2proc/app.js
+++ catlitter/lambda2proc/app.js
@@ -2,24 +2,33 @@
 
 const configurations = require( "./etc/config.json" );
 const L2P = require( "./includes/l2p/l2p" );
+const KinesaStream = require( "./includes/kinesalite/kinesaliteStreamClient" );
+
+const kinesisHandler = ( records ) => {
+	console.dir(
+		records,
+		{ depth : null }
+	);
+};
 
 const runLambdaProcess = ( options ) => {
 	let lambda = new L2P( options );
-	for( let i=0 ; i<3 ; i++ )
-		lambda.invoke( {}, {} );
-	lambda.resetContext();
-	for( let i=0 ; i<3 ; i++ )
-		lambda.invoke( {}, {} );
+	let stream = new KinesaStream( options.streamName );
+	stream.read( kinesisHandler );
 };
 
 let options = {
 	functionName : null,
+	streamName : null,
 	lambdasStorage : configurations.lambdasStorage
 };
 for( let i = 0  ; i < process.argv.length ; i++ ) {
 	switch( process.argv[ i ] ) {
 		case "--function-name":
 			options.functionName = process.argv[++i];
+			break;
+		case "--stream-name":
+			options.streamName = process.argv[++i];
 			break;
 	}
 }
