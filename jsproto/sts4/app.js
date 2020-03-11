21f8f36b106a90e01a3db20dc4d42a09e53a8746
Alberto Mesin <a.mesin@libero.it>
2020-03-11 23:24:23 +0100

    cose

--- jsproto/sts4/app.js
+++ jsproto/sts4/app.js
@@ -11,7 +11,7 @@ const endpoint = {
 
 //const JWT = "eyJ0eXAiOiJqd3QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImcwYzBCekw1b19HU3pmRUJDTTZXU0hsNGdfaGx0Q3g0dmlaem5WNkkzeXMifQ";
 const JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1lMiIsImlhdCI6MTU1MDU4MTA4NH0.WN5D-BFLypnuklvO3VFQ5ucDjBT68R2Yc-gj8AlkRAs";
-
+//const JWT = "CfDJ8GxAkp8EI99Dnt_yK-4wXwTyN49dJzdk8SdJSb_JMZrqjD2X858poxDdHvFSqux4ALJ6NO7JaycgB2baxF67W035nFgvLlBaowllt9FsPYGyqzbvlTVhAuogtsVKNfoR-IS-QjNmPLYghF2-zGJ5IZE";
 let tv = new TockenValidator( endpoint );
 
 tv.verifyJWTToken( JWT )
@@ -20,7 +20,11 @@ tv.verifyJWTToken( JWT )
             result,
             { depth : null }
         );
-} );
+    } )
+    .catch( error => {
+        console.log( "=== Error ===" );
+        console.dir( error, { depth : null } );
+    } );
 
 //tv.getJWKSKeyStore()
 //    .then( result => {
