7fa8135b3562b494d351df073a62ae7a6a66bcdd
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-03-13 15:48:09 +0100

    Clean comments.

--- jsproto/sts4/lib/wsServer.js
+++ jsproto/sts4/lib/wsServer.js
@@ -4,36 +4,6 @@ const webSocket = require( "ws" );
 const http = require( "http" );
 const https = require( "https" );
 const url = require( "url" );
-//const wssOptions = {
-//    port: 8080,
-//    perMessageDeflate: {
-//        zlibDeflateOptions: {
-//            // See zlib defaults.
-//            chunkSize: 1024,
-//            memLevel: 7,
-//            level: 3
-//        },
-//        zlibInflateOptions: {
-//            chunkSize: 10 * 1024
-//        },
-//        // Other options settable:
-//        clientNoContextTakeover: true, // Defaults to negotiated value.
-//        serverNoContextTakeover: true, // Defaults to negotiated value.
-//        serverMaxWindowBits: 10, // Defaults to negotiated value.
-//        // Below options specified as default values.
-//        concurrencyLimit: 10, // Limits zlib concurrency for perf.
-//        threshold: 1024 // Size (in bytes) below which messages
-//        // should not be compressed.
-//    }
-//};
-/*const options = {
-	port: 8080,
-	protocol: "https",
-	httpsOptions: {
-		cert: "",
-		key: "" // or Buffers... :-|
-	}
-};*/
 
 class WsReceiver {
 	constructor( options ) {
@@ -69,18 +39,7 @@ class WsReceiver {
 		let getData = url.parse( incomingMessage.url, true );
 		let headers = incomingMessage.headers;
 		console.log( "=== connection ===" );
-		console.dir(
-			incomingMessage.headers,
-			{ depth : null }
-		);
-		console.dir(
-			url.parse( incomingMessage.url, true ),
-			{ depth : 1 }
-		);
-		/*console.dir(
-			incomingMessage.headers,
-			{ depth : null }
-		);*/
+		// TODO: verify token.
 	}
 
 	static _messageHandler( message ) { // for debug prurpose at the moment...
@@ -97,23 +56,3 @@ class WsReceiver {
 }
 
 module.exports = WsReceiver;
-
-//const fs = require('fs');
-//const https = require('https');
-//const WebSocket = require('ws');
-// 
-//const server = https.createServer({
-//  cert: fs.readFileSync('/path/to/cert.pem'),
-//  key: fs.readFileSync('/path/to/key.pem')
-//});
-//const wss = new WebSocket.Server({ server });
-// 
-//wss.on('connection', function connection(ws) {
-//  ws.on('message', function incoming(message) {
-//    console.log('received: %s', message);
-//  });
-// 
-//  ws.send('something');
-//});
-// 
-//server.listen(8080);
