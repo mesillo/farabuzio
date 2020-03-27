b4ae7a1c707e690abe477b44098a05d16a11d3b6
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-03-27 18:17:29 +0100

    stocazzio

--- jsproto/sts4/testSocketIO/client.js
+++ jsproto/sts4/testSocketIO/client.js
@@ -2,23 +2,47 @@
 
 const io = require( "socket.io-client" );
 
+//const JWT = "someJWTstring";
+const JWT = "";
+
+//let socket = io( "http://localhost:9010/websoket", {} );
+//let socket = io( "http://localhost:9010/websoket?tocken=" + JWT );
 let socket = io( "http://localhost:9010", {
-  path: "/websoket"
+	path: "/websocket",
+	query: {
+		tocken: JWT
+	}
 } );
 
 socket.on( "connect", () => {
-    console.log( "Connected to the server." );
-    //console.dir( arguments, { depth: 0 } );
-    //socket.emit( "handshake", { message: "An handshake message" } );
-    socket.on( "pingevent", ( payload ) => {
-        console.dir(
-            payload,
-            { depth: null }
-        );
-        socket.emit( "handshake", { message: "An handshake message" } );
-    } );
-    socket.on( "message", ( message ) => {
-        console.info( "Message from server:" + message );
-        //console.dir( arguments, { depth: 0 } );
-    } );
+	console.log( "Connected to the server." );
+	//console.dir( arguments, { depth: 0 } );
+	//socket.emit( "handshake", { message: "An handshake message" } );
+	//socket.on( "pingevent", ( payload ) => {
+	//    console.dir(
+	//        payload,
+	//        { depth: null }
+	//    );
+	//    socket.emit( "handshake", { message: "An handshake message" } );
+	//} );
+	socket.emit( "handshake", { message: "An handshake message" } );
+	dataProducer( 1000 );
+	/*socket.on( "message", ( message ) => {
+		console.info( "Message from server:" + message );
+		//console.dir( arguments, { depth: 0 } );
+	} );*/
 } );
+
+socket.on( "disconnect", () => {
+	console.log( "Disconnected from server" );
+	console.dir( arguments, { depth : 0 } );
+	process.exit();
+} );
+
+let dataProducer = ( delay ) => {
+	setTimeout( () => {
+		socket.emit( "data", { data : "somedata: " + delay } );
+		delay *= 2;
+		dataProducer( delay );
+	}, delay );
+}
--- jsproto/sts4/testSocketIO/server.js
+++ jsproto/sts4/testSocketIO/server.js
@@ -3,42 +3,84 @@
 const socketio = require( "socket.io" );
 const http = require( "http" );
 const https = require( "https" );
+const url = require( "url" );
 
 const ioOptions = {
-    serveClient: false,
-    cookie: false,
-    path: "/websoket"
+	serveClient: false,
+	cookie: false,
+	path: "/websocket"
 };
 const generalOption = {
-    port: 9010,
-    agent: http,
-    agentOptions: {}
+	port: 9010,
+	agent: http,
+	agentOptions: {}
 };
 
-class protocolManager {
-    constructor( socket ) {
-        this.socket = socket;
-        this._setUpEvents();
-    }
+class ProtocolManager {
+	constructor( socket, tockenInfo ) {
+		this.socket = socket;
+		this.tockenInfo = tockenInfo;
+		this._setUpEvents();
+	}
 
-    _setUpEvents() {
+	_setUpEvents() {
 		this.socket.on( "message", ( message ) => {
+			console.log( "=== generic Message from client ===" );
+			console.log( "===================================" );
 			console.log( message );
 		} );
 		this.socket.on( "handshake", ( message ) => {
+			console.log( "=== Handshake from client ===" );
+			console.dir( message );
+			//this.socket.emit( "ack", { ackdata : "somedata" } ); //TODo: test this... this... 
+		} );
+		this.socket.on( "data", ( message ) => {
+			console.log( "=== Data from client ===" );
 			console.dir( message );
 		} );
 	}
 }
 
+let httpRequestListener = ( request, response ) => {
+	console.log( "=== HTTP Server ===" );
+	const urlObj = url.parse( request.url, true );
+	console.dir( urlObj.query );
+	console.log( "===================" );
+};
+
+//let webServer = generalOption.agent.createServer( generalOption.agentOptions, httpRequestListener );
 let webServer = generalOption.agent.createServer( generalOption.agentOptions );
 let io = socketio( webServer, ioOptions );
 
+io.use( ( socket, next ) => {
+	console.log( "==== MIDDLEWARE ====" );
+	//let queryObj = url.parse( socket.handshake.query, true );
+	//console.dir( socket.handshake.query );
+	//console.dir( socket.request );
+	//console.dir( socket.request.connection );
+	if( ! socket.handshake.query.tocken || socket.handshake.query.tocken === "" ) {
+		console.log( "== Unauthorized ==" );
+
+		next( new Error( "Authentication error") );
+		socket.disconnect( true );
+	} else {
+	// TODO: e di next che me ne faccio??? Puoi passargli error...
+		next();
+	}
+} );
+
 io.on( "connect", ( socket ) => {
 	console.info( "Server get connection from " + socket.id );
-	socket.emit( "pingevent", { ping: 0 } );
+	//socket.emit( "pingevent", { ping: 0 } );
+	//console.dir( socket.handshake.query );
+	//if( ! socket.handshake.query.tocken || socket.handshake.query.tocken === "" ) {
+	//	console.log( "== Unauthorized ==" );
+	//	socket.disconnect( true );
+	//}
+	let tockenInfo = socket.handshake.query.tocken;
+	let protocolManager = new ProtocolManager( socket, tockenInfo );
 	//socket.send( "Welcome " +  socket.id + "!" );
-    //console.dir( arguments );
+	//console.dir( arguments );
 } );
 
 webServer.listen( generalOption.port );
