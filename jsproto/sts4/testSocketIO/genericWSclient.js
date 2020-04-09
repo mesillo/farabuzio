ed138a6eb7dca81c4baea8e8d4a176d2a721522f
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-04-09 19:22:29 +0200

    cose clientose.

--- jsproto/sts4/testSocketIO/genericWSclient.js
+++ jsproto/sts4/testSocketIO/genericWSclient.js
@@ -2,7 +2,7 @@
 
 const io = require( "socket.io-client" );
 
-const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODYzNTg4NTMsImV4cCI6MTU4NjQ0NTI1MywiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MGE2ODYzMzY2YzUuZXUuZm0tY2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vdG9rZW4uc3RzdjQuNTBhNjg2MzM2NmM1LmV1LmZtLWNsb3VkLmNvbS9yZXNvdXJjZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImNsaWVudF9pZCI6Im1hc3RlciIsInN1YiI6IjkxMzI2N2I4LTU0MDUtNDk4ZC03NjE4LTA4ZDc2OWJiMWY4ZiIsImF1dGhfdGltZSI6MTU4NjM1ODg1MywiaWRwIjoibG9jYWwiLCJyZXZlYWxfYWNjb3VudF9pZCI6IjEwMTUxMjciLCJyZXZlYWxfdXNlcl9pZCI6IjE1OTgwMjgiLCJyZXZlYWxfdXNlcl90eXBlX2lkIjoiMiIsInVuaXF1ZV9uYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzQFRFdGVzdC5jb20iLCJuYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwianRpIjoiZThkNDZkNzVmNmMzMjkwMTUwZjJkZDE4NDVhNjQ3MDMiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImFtciI6WyJwd2QiXX0.g25xLI-B33fENow4RSWcKxyTNE7FKDbxk_EZ-tuT2enAdINC4N3NbzoCXO04Ssb7dCWVSkyxl98ksHmZTpa7OW0NJtM4XuPKCCaErtVPdtG8yvNLSrqG7YPFwqG_SlJgCBYvxNItCQb2G7dPkWAjRvNIlEgrmrDtQ51pd8ObhalIR2azH83E_N8TEGLF4KJUVoSNmmL2p_eYFrfcHJa7xEPyXy0EFcaFVQXxBMz1UdJTDhQgt6BbamnnGCLaLRZqrOeHvLaJp964N5iMyKefsQqz3I3CNE6NOX4z9PIdDf21awwSSM6U9ZdgPkt0O5tzd1K3Ns_NzUreQ6s77kRlYd55Z_JtNkxRCkSnJkJ-_T3gzoPSlghsamnrFf4sMtVaprGHBw-dKVSOEsREIJ352Dj5ZaV9DLSnvvZFYiQrbEUOeCwRN7Yu56f13oR7drM-rBOY0gS-OPSPlHUCz24WTtfzuS9vmQjbMdwAzjflNjwPIMFZiQLEsNqU5TQdCxnwgCHpjNiPTQ-ezVR1ntIaRfYPUsYJpL0cmHCwAiA44GsEDyPxS-kOaRB_WxB_c2qqfLEHKHX3MAYXfxD96QWAbbEMZB5RRq1OvSxDLpe6ylDICFFXXj1Z__8vKIPIuRtOXIlXday5UtKvmKdfSsNBrFeU1F12dtZ2k7P77PSmWXE";
+const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODY0NDU2NTUsImV4cCI6MTU4NjUzMjA1NSwiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MGE2ODYzMzY2YzUuZXUuZm0tY2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vdG9rZW4uc3RzdjQuNTBhNjg2MzM2NmM1LmV1LmZtLWNsb3VkLmNvbS9yZXNvdXJjZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImNsaWVudF9pZCI6Im1hc3RlciIsInN1YiI6IjkxMzI2N2I4LTU0MDUtNDk4ZC03NjE4LTA4ZDc2OWJiMWY4ZiIsImF1dGhfdGltZSI6MTU4NjQ0NTY1NSwiaWRwIjoibG9jYWwiLCJyZXZlYWxfYWNjb3VudF9pZCI6IjEwMTUxMjciLCJyZXZlYWxfdXNlcl9pZCI6IjE1OTgwMjgiLCJyZXZlYWxfdXNlcl90eXBlX2lkIjoiMiIsInVuaXF1ZV9uYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzQFRFdGVzdC5jb20iLCJuYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwianRpIjoiZTliZDk1ZjViM2Y3ZDI3OTIzYTUyODZmMzIyNWFhZjYiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImFtciI6WyJwd2QiXX0.bT_U0j34p7htpvFYyoDOM18rYPFQKse0y2TTS86HWWv76et-vu96agjy_chhUt6okifd_VKFuCv_wQllXdX3AL1_tC31sd8iVdbasK7w9Alt_NpYKcYPNQwnlj4-AQbev-h5wkSBTa7mbFM1OtQF7-DAxc9iF5WvypEhDTXzn9OeOIvF8MdXK_5ckj8IFuLzFZsYcg5LU3nPlH3livoWR9ba_e3ZKVUnDi-X0RfCcvkQF4dxVZq0rdba00lJ9kAzJSP8QapoB_lJ_OhB8lHX5w93L7Wdn6F29Zylrbv4_Q6LP7S7Zye05xbyaQm1a66kDalwcQ6AG1wxE5R18ipTG6AVvoYaghpLR9p0vAB3k0H_npnfHYn51ajPDbjTaUVmQL5NhzqyFIiO_MPo4y0jpSG7wZ3-UUUbo40JuWsQlpWf5hY_UVLyk5QC5woqrvQtkwEqhpZ2ECH2wk3h2lbRQoAAE5tm9wgzbOCGttuDkjWycbRc9tlDVucIW4DTNTDZEEsrZdhX-cr1tUSUzKyO8peAbZLg9BP7AhGTuerQu3gp72iLRPzNZh4tZzM5MHBIMoqudcWxhf2c31DX4XyJshMfOdvQsEKvL47QtyyHsBAzdAtnMhfQtFb1sM1ly1bMtTXmugk29Us6LQ8habxEoiMEinrIBkkQIZLLT0M_go0";
 const DATA_DELAY = 2500;
 
 const socket = io( "http://localhost:9010", { // https is supported and can be used.
@@ -12,36 +12,6 @@ const socket = io( "http://localhost:9010", { // https is supported and can be u
 	}
 } );
 
-/*
-[
-	"startSession",  {
-	  thingType: "20",
-	  clientProtocolVersion: "1",
-	  id: "",
-	  schema: {
-		 position:{
-			 dynamic: [
-			 "Header.UpdateTime",
-			 "Gps.Latitude",
-			 "Gps.Longitude",
-			 ....   
-			 ],
-			 static: {
-				 "Mobile.OSVersion": 4,
-				 "Mobile.AppName": 1,
-				 ....
-			 },
-			 token: {
-				 "reveal_user_id" : "Header.ThingId",
-				 "reveal_user_id": "Header.AccountId",
-				 ...
-			 }
-		 ...
-		 }
-	  }
-	}
- ]
-*/
 // 1) if the server cant validate the token it emit a "error" event an, immediatly after, close the soket.
 // So client must listen to this events
 socket.on( "error", ( error ) => {
@@ -58,38 +28,40 @@ let id = 0;
 class Messages {
 	static startSession( socket ) {
 		socket.emit( "startSession", {
-			thingId: "00001",
 	 		thingType: "20",
-	 		os: "to-be-defined",
-	 		osVersion: "to-be-defined",
-	 		appName: "FSD",
-	 		appVersion: "1",
-	 		clientProtocolVersion: "1",
+			clientProtocolVersion: "1",
+			id: id.toString(),
 	 		schema: {
-				position: [ // name of data schema.
-					"Header.UpdateTime",
-					"Header.AccountId",
-					"Header.SequenceNumber",
-					"GPS.Latitude",
-					"GPS.Longitude",
-					"GPS.FixQuality",
-					"GPS.Speed",
-					"GPS.Heading",
-					"GPS.Altitude",
-					"Radio.GsmSignalLevel",
-					"Radio.GsmCSQ",
-					"PowerSupply.BatVoltage",
-					"Mobile.EventType",
-					"Mobile.CorrelationId",
-					"Mobile.DeviceType",
-					"Mobile.DeviceId",
-					"Mobile.OS",
-					"Mobile.OSVersion",
-					"Mobile.AppName",
-					"Mobile.AppVersion"
-				]
-			},
-			id: id.toString()
+				position: {
+					dynamic: [ // name of data schema.
+						"Header.UpdateTime",
+						"Header.SequenceNumber",
+						"GPS.Latitude",
+						"GPS.Longitude",
+						"GPS.FixQuality",
+						"GPS.Speed",
+						"GPS.Heading",
+						"GPS.Altitude",
+						"Radio.GsmSignalLevel",
+						"Radio.GsmCSQ",
+						"PowerSupply.BatVoltage",
+						"Mobile.EventType",
+						"Mobile.CorrelationId"
+					],
+					static: {
+						"Mobile.DeviceType": "static-devicetype",
+						"Mobile.DeviceId": "static-deviceid",
+						"Mobile.OS": "WednesdayOS",
+						"Mobile.OSVersion": "007",
+						"Mobile.AppName": "static-FSD",
+						"Mobile.AppVersion": "example"
+					},
+					token: { // Tocken info : standard decoded value to add on messages.
+						"reveal_user_id": "Header.ThingId",
+						"reveal_account_id": "Header.AccountId"
+					}
+				}
+			}
 		},
 		( event, payload ) => {
 			if( event === "sessionEstablished" && payload.id === id.toString() ) {
@@ -114,7 +86,6 @@ class Messages {
 			id: stringId,
 			data: [
 			(new Date()).toISOString(), //"Header.UpdateTime",
-			123456, //"Header.AccountId",
 			stringId, //"Header.SequenceNumber",
 			44.9877331, //"GPS.Latitude",
 			12.0287777, //"GPS.Longitude",
@@ -126,16 +97,11 @@ class Messages {
 			33, //"Radio.GsmCSQ",
 			12, //"PowerSupply.BatVoltage",
 			15, //"Mobile.EventType",
-			"idontknow", //"Mobile.CorrelationId",
-			"devicetype", //"Mobile.DeviceType",
-			"deviceid", //"Mobile.DeviceId",
-			"adroid", //"Mobile.OS",
-			"10", //"Mobile.OSVersion",
-			"FSD", //"Mobile.AppName",
-			"1" //Mobile.AppVersion"
+			"idontknow" //"Mobile.CorrelationId",
 		] },
 		( event, payload ) => { // 5) check receiving
 			if( event === "ack" && payload.id === stringId ) {
+				console.log( "Ack pkt " + stringId );
 				// packet acked
 			} else if( event === "nack" ) {
 				// packet nacked; retry o discard.
---
+++ jsproto/sts4/testSocketIO/genericWSclient_oldHandshake.js
@@ -0,0 +1,154 @@
+"use strict";
+
+const io = require( "socket.io-client" );
+
+const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODYzNTg4NTMsImV4cCI6MTU4NjQ0NTI1MywiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MGE2ODYzMzY2YzUuZXUuZm0tY2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vdG9rZW4uc3RzdjQuNTBhNjg2MzM2NmM1LmV1LmZtLWNsb3VkLmNvbS9yZXNvdXJjZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImNsaWVudF9pZCI6Im1hc3RlciIsInN1YiI6IjkxMzI2N2I4LTU0MDUtNDk4ZC03NjE4LTA4ZDc2OWJiMWY4ZiIsImF1dGhfdGltZSI6MTU4NjM1ODg1MywiaWRwIjoibG9jYWwiLCJyZXZlYWxfYWNjb3VudF9pZCI6IjEwMTUxMjciLCJyZXZlYWxfdXNlcl9pZCI6IjE1OTgwMjgiLCJyZXZlYWxfdXNlcl90eXBlX2lkIjoiMiIsInVuaXF1ZV9uYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzQFRFdGVzdC5jb20iLCJuYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwianRpIjoiZThkNDZkNzVmNmMzMjkwMTUwZjJkZDE4NDVhNjQ3MDMiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImFtciI6WyJwd2QiXX0.g25xLI-B33fENow4RSWcKxyTNE7FKDbxk_EZ-tuT2enAdINC4N3NbzoCXO04Ssb7dCWVSkyxl98ksHmZTpa7OW0NJtM4XuPKCCaErtVPdtG8yvNLSrqG7YPFwqG_SlJgCBYvxNItCQb2G7dPkWAjRvNIlEgrmrDtQ51pd8ObhalIR2azH83E_N8TEGLF4KJUVoSNmmL2p_eYFrfcHJa7xEPyXy0EFcaFVQXxBMz1UdJTDhQgt6BbamnnGCLaLRZqrOeHvLaJp964N5iMyKefsQqz3I3CNE6NOX4z9PIdDf21awwSSM6U9ZdgPkt0O5tzd1K3Ns_NzUreQ6s77kRlYd55Z_JtNkxRCkSnJkJ-_T3gzoPSlghsamnrFf4sMtVaprGHBw-dKVSOEsREIJ352Dj5ZaV9DLSnvvZFYiQrbEUOeCwRN7Yu56f13oR7drM-rBOY0gS-OPSPlHUCz24WTtfzuS9vmQjbMdwAzjflNjwPIMFZiQLEsNqU5TQdCxnwgCHpjNiPTQ-ezVR1ntIaRfYPUsYJpL0cmHCwAiA44GsEDyPxS-kOaRB_WxB_c2qqfLEHKHX3MAYXfxD96QWAbbEMZB5RRq1OvSxDLpe6ylDICFFXXj1Z__8vKIPIuRtOXIlXday5UtKvmKdfSsNBrFeU1F12dtZ2k7P77PSmWXE";
+const DATA_DELAY = 2500;
+
+const socket = io( "http://localhost:9010", { // https is supported and can be used.
+	path: "/ws-receiver", // listening path is configurable.
+	query: {
+		tocken: JWT
+	}
+} );
+
+/*
+[
+	"startSession",  {
+	  thingType: "20",
+	  clientProtocolVersion: "1",
+	  id: "",
+	  schema: {
+		 position:{
+			 dynamic: [
+			 "Header.UpdateTime",
+			 "Gps.Latitude",
+			 "Gps.Longitude",
+			 ....   
+			 ],
+			 static: {
+				 "Mobile.OSVersion": 4,
+				 "Mobile.AppName": 1,
+				 ....
+			 },
+			 token: {
+				 "reveal_user_id" : "Header.ThingId",
+				 "reveal_account_id": "Header.AccountId",
+				 ...
+			 }
+		 ...
+		 }
+	  }
+	}
+ ]
+*/
+// 1) if the server cant validate the token it emit a "error" event an, immediatly after, close the soket.
+// So client must listen to this events
+socket.on( "error", ( error ) => {
+	console.log( "== ERROR ==" );
+	console.dir( error );
+} );
+socket.on( "disconnect", () => {
+	console.log( "== Disconnected from server ==" );
+	process.exit();
+} );
+
+let id = 0;
+
+class Messages {
+	static startSession( socket ) {
+		socket.emit( "startSession", {
+			thingId: "00001",
+	 		thingType: "20",
+	 		os: "to-be-defined",
+	 		osVersion: "to-be-defined",
+	 		appName: "FSD",
+	 		appVersion: "1",
+	 		clientProtocolVersion: "1",
+	 		schema: {
+				position: [ // name of data schema.
+					"Header.UpdateTime",
+					"Header.AccountId",
+					"Header.SequenceNumber",
+					"GPS.Latitude",
+					"GPS.Longitude",
+					"GPS.FixQuality",
+					"GPS.Speed",
+					"GPS.Heading",
+					"GPS.Altitude",
+					"Radio.GsmSignalLevel",
+					"Radio.GsmCSQ",
+					"PowerSupply.BatVoltage",
+					"Mobile.EventType",
+					"Mobile.CorrelationId",
+					"Mobile.DeviceType",
+					"Mobile.DeviceId",
+					"Mobile.OS",
+					"Mobile.OSVersion",
+					"Mobile.AppName",
+					"Mobile.AppVersion"
+				]
+			},
+			id: id.toString()
+		},
+		( event, payload ) => {
+			if( event === "sessionEstablished" && payload.id === id.toString() ) {
+				// ack received, session estabilished.
+				console.log( "= sessionEstablished =" );
+				// 4) start transmita data (in this case position)
+				setInterval( ( ) => {
+					Messages.sendPosition( socket );
+				}, DATA_DELAY);
+			} else if( event === "sessionFailed" ) {
+				// nack received, session failed.
+				console.log( "sessionFailed: " + payload.error );
+				process.exit();
+			}
+		}
+		);
+	}
+
+	static sendPosition( socket ) {
+		let stringId = (++id).toString();
+		socket.emit( "position", {
+			id: stringId,
+			data: [
+			(new Date()).toISOString(), //"Header.UpdateTime",
+			123456, //"Header.AccountId",
+			stringId, //"Header.SequenceNumber",
+			44.9877331, //"GPS.Latitude",
+			12.0287777, //"GPS.Longitude",
+			100, //"GPS.FixQuality",
+			80, //"GPS.Speed",
+			0.12, //"GPS.Heading",
+			10, //"GPS.Altitude",
+			22, //"Radio.GsmSignalLevel",
+			33, //"Radio.GsmCSQ",
+			12, //"PowerSupply.BatVoltage",
+			15, //"Mobile.EventType",
+			"idontknow", //"Mobile.CorrelationId",
+			"devicetype", //"Mobile.DeviceType",
+			"deviceid", //"Mobile.DeviceId",
+			"adroid", //"Mobile.OS",
+			"10", //"Mobile.OSVersion",
+			"FSD", //"Mobile.AppName",
+			"1" //Mobile.AppVersion"
+		] },
+		( event, payload ) => { // 5) check receiving
+			if( event === "ack" && payload.id === stringId ) {
+				// packet acked
+			} else if( event === "nack" ) {
+				// packet nacked; retry o discard.
+				console.error( payload.error );
+				process.exit();
+			}
+		} );
+	} 
+}
+
+// 2) when the server validate the token the "connect" event is raised.
+socket.on( "connect", () => {
+	console.log( "== Connected to the server ==" );
+	// 3) now client can emit a "startSession" event
+	Messages.startSession( socket );
+} );
