657eacfea6d2a5e6e37411e4d30911a0e0514fa1
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-04-20 19:31:18 +0200

    biscotti mannari dallo spazio profondo.

--- jsproto/sts4/testSocketIO/genericWSclient.js
+++ jsproto/sts4/testSocketIO/genericWSclient.js
@@ -5,6 +5,13 @@ const io = require( "socket.io-client" );
 const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODczNjk0ODUsImV4cCI6MTU4NzQ1NTg4NSwiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MGE2ODYzMzY2YzUuZXUuZm0tY2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vdG9rZW4uc3RzdjQuNTBhNjg2MzM2NmM1LmV1LmZtLWNsb3VkLmNvbS9yZXNvdXJjZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImNsaWVudF9pZCI6Im1hc3RlciIsInN1YiI6IjkxMzI2N2I4LTU0MDUtNDk4ZC03NjE4LTA4ZDc2OWJiMWY4ZiIsImF1dGhfdGltZSI6MTU4NzM2OTQ4NSwiaWRwIjoibG9jYWwiLCJyZXZlYWxfYWNjb3VudF9pZCI6IjEwMTUxMjciLCJyZXZlYWxfdXNlcl9pZCI6IjE1OTgwMjgiLCJyZXZlYWxfdXNlcl90eXBlX2lkIjoiMiIsInVuaXF1ZV9uYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzQFRFdGVzdC5jb20iLCJuYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwianRpIjoiNmIwMmY5MWUyNTRiNTg2YWNmYjdiYzgxMzU2MTdiN2QiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImFtciI6WyJwd2QiXX0.jb100niNnywTO2FVVT8c2ZyWmsAtJE9kC-wheIQvK3WDDURYYE16oZlfPL0c_qTE0aU6HgaKUO_LCamdWbk-A9qbA8NyZLq3gN7J_3_bMwR_-Lelhg0lMNoc9ngiGC9t_U06cJW_gtD0wFfByW9RcacN2ALAo3SqFz6MBiEXOTDldNxChs1CxAKGrUot0sLIDdxusXPOsCvuR-BiimrtEjAo6ucIS8-rvuZyprvKDrNgDS2zLXbcTn36UHyNzj7wv54tjqxnsuYP6TDTIKkAXQuJPKpDM8abR4s8GbhZTs5uQc5U2orLaNct3PHz6HQUYllC1c5rk0nXy466WBXgeqqMQdLLAEg57HCfwUVebSYki3QAkTf87IeAh0FCZ7hbZIaomnGycWK5nD59D0eyB8J4HPT5AKz7YOUeOhDK7RFaChbovRFjWL32LY809cmD30RXb74WhjnvzCimXHK6O4PezzCP30zqwMb-Aj_CWyKg28ewIRrBzorPcZTFReaR5-ykIBmDMV-XQvfeT_QRWfc0bJpEqvioOdmZ3v2xeo4aejbWrUpMf1Qg78FY--LANacne0334U82bZ2aKvKI7Y789bGBCWvjUUBASTUjHvWgDEOJAZjKPv_Tm9R7Zzpfz8N7HhxgChKIQX4iQ5ScdNgQD5XS45uQVupzKXhO0DE";
 const DATA_DELAY = 2500;
 ///////
+let rawCookies = null;
+let parsedCoockies = null;
+
+const prepareCoockie = () => {
+
+};
+
 const https = require( "https" );
 const options = {
   hostname: "genericws.dev.smb.vzc-iot.com",
@@ -15,18 +22,25 @@ const options = {
 
 const request = https.request( options, ( response ) => {
 	//console.log( `statusCode: ${response.statusCode}` ); //if 200
-	console.dir( response.headers["set-cookie"] );
-	response.on( "data", data => {
-		process.stdout.write( data );
-	} );
+	//console.dir( response.headers["set-cookie"] );
+	//response.on( "data", data => {
+	//	process.stdout.write( data );
+	//} );
+	if( response.statusCode === 200 ) {
+		rawCookies = response.headers["set-cookie"];
+		prepareCoockie();
+	} else {
+		console.error( `statusCode: ${response.statusCode}` );
+		process.exit( 1 );
+	}
 });
 
 request.on( "error", ( error ) => {
 	console.error( error );
+	process.exit( 1 );
 } );
 
 request.end();
-return;
 ///////
 //const socket = io( "http://localhost:9011", { // https is supported and can be used.
 const socket = io( "https://genericws.dev.smb.vzc-iot.com:443", {
@@ -36,6 +50,13 @@ const socket = io( "https://genericws.dev.smb.vzc-iot.com:443", {
 	},
 	transports: [ "websocket" ],
 	//rejectUnauthorized: false // self-signed server cert,
+	transportOptions: {
+		polling: {
+			extraHeaders: {
+				"Cookie": "hello=world"
+			}
+		}
+	}
 } );
 
 // 1) if the server cant validate the token it emit a "error" event an, immediatly after, close the soket.
