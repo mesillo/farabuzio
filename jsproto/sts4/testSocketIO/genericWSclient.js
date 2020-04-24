cd5c0f2fbaab16c79c594e11b29d703d8fd8fb8d
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-04-24 14:44:06 +0200

    Cose puzzose.

--- jsproto/sts4/testSocketIO/genericWSclient.js
+++ jsproto/sts4/testSocketIO/genericWSclient.js
@@ -1,172 +1,188 @@
 "use strict";
 
 const io = require( "socket.io-client" );
+const https = require( "https" );
 
-const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODczNjk0ODUsImV4cCI6MTU4NzQ1NTg4NSwiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MGE2ODYzMzY2YzUuZXUuZm0tY2xvdWQuY29tIiwiYXVkIjpbImh0dHBzOi8vdG9rZW4uc3RzdjQuNTBhNjg2MzM2NmM1LmV1LmZtLWNsb3VkLmNvbS9yZXNvdXJjZXMiLCJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImNsaWVudF9pZCI6Im1hc3RlciIsInN1YiI6IjkxMzI2N2I4LTU0MDUtNDk4ZC03NjE4LTA4ZDc2OWJiMWY4ZiIsImF1dGhfdGltZSI6MTU4NzM2OTQ4NSwiaWRwIjoibG9jYWwiLCJyZXZlYWxfYWNjb3VudF9pZCI6IjEwMTUxMjciLCJyZXZlYWxfdXNlcl9pZCI6IjE1OTgwMjgiLCJyZXZlYWxfdXNlcl90eXBlX2lkIjoiMiIsInVuaXF1ZV9uYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzQFRFdGVzdC5jb20iLCJuYW1lIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsIjoibHVjYXNAVEV0ZXN0LmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwianRpIjoiNmIwMmY5MWUyNTRiNTg2YWNmYjdiYzgxMzU2MTdiN2QiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicmV2ZWFsIl0sImFtciI6WyJwd2QiXX0.jb100niNnywTO2FVVT8c2ZyWmsAtJE9kC-wheIQvK3WDDURYYE16oZlfPL0c_qTE0aU6HgaKUO_LCamdWbk-A9qbA8NyZLq3gN7J_3_bMwR_-Lelhg0lMNoc9ngiGC9t_U06cJW_gtD0wFfByW9RcacN2ALAo3SqFz6MBiEXOTDldNxChs1CxAKGrUot0sLIDdxusXPOsCvuR-BiimrtEjAo6ucIS8-rvuZyprvKDrNgDS2zLXbcTn36UHyNzj7wv54tjqxnsuYP6TDTIKkAXQuJPKpDM8abR4s8GbhZTs5uQc5U2orLaNct3PHz6HQUYllC1c5rk0nXy466WBXgeqqMQdLLAEg57HCfwUVebSYki3QAkTf87IeAh0FCZ7hbZIaomnGycWK5nD59D0eyB8J4HPT5AKz7YOUeOhDK7RFaChbovRFjWL32LY809cmD30RXb74WhjnvzCimXHK6O4PezzCP30zqwMb-Aj_CWyKg28ewIRrBzorPcZTFReaR5-ykIBmDMV-XQvfeT_QRWfc0bJpEqvioOdmZ3v2xeo4aejbWrUpMf1Qg78FY--LANacne0334U82bZ2aKvKI7Y789bGBCWvjUUBASTUjHvWgDEOJAZjKPv_Tm9R7Zzpfz8N7HhxgChKIQX4iQ5ScdNgQD5XS45uQVupzKXhO0DE";
+const JWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM5RDcxN0Y3QkI3RjE1QUEzMDBCNzRENTA0QzExRjA4REM4RjBBMUQiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJPZGNYOTd0X0Zhb3dDM1RWQk1FZkNOeVBDaDAifQ.eyJuYmYiOjE1ODc1NjUxNTIsImV4cCI6MTU4NzY1MTU1MiwiaXNzIjoiaHR0cHM6Ly90b2tlbi5zdHN2NC41MDNhY2E0MWEwOGQudGVzdC51cy5mbS1jbG91ZC5jb20iLCJhdWQiOlsiaHR0cHM6Ly90b2tlbi5zdHN2NC41MDNhY2E0MWEwOGQudGVzdC51cy5mbS1jbG91ZC5jb20vcmVzb3VyY2VzIiwib3BlbmlkIiwicHJvZmlsZSIsInJldmVhbCJdLCJjbGllbnRfaWQiOiJtYXN0ZXIiLCJzdWIiOiI0YTE1MTA5YS01Y2UzLTRhNTAtMGUxZi0wOGQ3M2M2ZTRiOTciLCJhdXRoX3RpbWUiOjE1ODc1NjUxNTIsImlkcCI6ImxvY2FsIiwicmV2ZWFsX2FjY291bnRfaWQiOiIxMTI4NDc2IiwicmV2ZWFsX3VzZXJfaWQiOiI1MDI2MjEyIiwicmV2ZWFsX3VzZXJfdHlwZV9pZCI6IjEiLCJ1bmlxdWVfbmFtZSI6ImFuYXN0YXNzaWFAdmVyaXpvbmNvbm5lY3QuY29tIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYW5hc3Rhc3NpYUB2ZXJpem9uY29ubmVjdC5jb20iLCJuYW1lIjoiYW5hc3Rhc3NpYUB2ZXJpem9uY29ubmVjdC5jb20iLCJlbWFpbCI6ImFuYXN0YXNzaWFAdmVyaXpvbmNvbm5lY3QuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJqdGkiOiI5YmE4NjZjODJjMDg1MTg0YmQ5NWI0NWYwZWJlZDJhYSIsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJyZXZlYWwiXSwiYW1yIjpbInB3ZCJdfQ.ORFJeZQAQmDq6NEt_rlbd4idNZfluSW_2PYFQJcgskr21FU94TIrvPDPntgt771pTa2gXwkG0xXYqL2akCt1lIaP7jMWXL2clrERFVN8eyvJcCRT9LWohUp32GY25XFY871fhFFA16UGKD8AMo147yK5Y9W3H5LOJN8tI7JCHwecrm7NsYvIHrt3RzWvShi0uwgeblqqkMtwYvYdlVJFq70qfC7riu_nt0I8F-Qe1XQPQE7WW7OVvXJkvJwFEUrOaDW22yTE6LQWlI5VlSdsz--pvJ5wVkbHKN0qVwl4T3Lx_5KeST6dka3ohWu1YX8Kpz1CvOD4mwP6SxAX4Z4UdBATbrBC2XWD_6rRnq-Xp-8jMurR6iGsAU5HWItRuY4Z2mi2FzxCQHVQVvRBD8d4j9FIpC_1h-2kkU25TkvYqQmniTsFXCz4Xyyk6ou86tIVo9pZ7yC5UaITTNCGgiV_1mj2BVgfz2325CEN8wh27TlICzp80xH8-lsirzN15sJBRYTPK2ws-fStyi7rJllwic_MEyQcF0RHtwO4l73ifTksUnXUXcejLGLuQsWTvKzU2exOfRzCbWY_NYdd9OIsLqqn1it_YRLyjK2Y_RGcnlPFjA8REUW4NeZV8G2RTVhzeBmr9lFr3fHiLW3hXXuUPTYR5E8O0kqZvXU28-P8EBk";
 const DATA_DELAY = 2500;
-///////
-let rawCookies = null;
-let parsedCoockies = null;
 
-const prepareCoockie = () => {
+// 0) Getting Cookies from ALB (Load balancer)... this is not a part of protocol but a method to simulate the behavior of a Browser/WebView in NodeJS environment.
+// AWS ALB is designed to deal with Web Clients.
+const options = {
+	hostname: "genericws.dev.smb.vzc-iot.com", // The ALB hostname
+	port: 443,
+	path: "/status", // The HealthCheck path
+	method: "GET"
+  };
+
+const prepareCoockie = ( rawCookies ) => { // Obtain Cookie Browser-Like string from the "set-cookie" ALB (server) response.
 	let cookieString = "";
 	for( let setC of rawCookies ) {
 		cookieString += setC.split( " " )[0] + " ";
 	}
-	parsedCoockies = cookieString;
+	return cookieString;
 };
 
-const https = require( "https" );
-const options = {
-  hostname: "genericws.dev.smb.vzc-iot.com",
-  port: 443,
-  path: "/status",
-  method: "GET"
+const getCookieFormALB = ( options ) => { // Query the ALB HealthCheck path for obtain Cookies
+	return new Promise( ( resolve, reject ) => {
+		const request = https.request( options, ( response ) => {
+			if( response.statusCode === 200 ) {
+				resolve( prepareCoockie( response.headers["set-cookie"] ) );
+			} else {
+				reject( `statusCode: ${response.statusCode}` );
+			}
+		});
+
+		request.on( "error", ( error ) => {
+			reject( error );
+		} );
+
+		request.end();
+	} );
 };
 
-const request = https.request( options, ( response ) => {
-	//console.log( `statusCode: ${response.statusCode}` ); //if 200
-	//console.dir( response.headers["set-cookie"] );
-	//response.on( "data", data => {
-	//	process.stdout.write( data );
-	//} );
-	if( response.statusCode === 200 ) {
-		rawCookies = response.headers["set-cookie"];
-		prepareCoockie();
-	} else {
-		console.error( `statusCode: ${response.statusCode}` );
-		process.exit( 1 );
-	}
-});
-
-request.on( "error", ( error ) => {
+getCookieFormALB( options ).then( ( parsedCoockies ) => {
+	runClient( parsedCoockies ); // Start the Socket.IO client application with ALB Cookies.
+	//runClient( null );
+	// If you will run the client in a Browser/WebView Like environment the parsedCoockies options nust be null or undefined.
+} ).catch( ( error ) => {
 	console.error( error );
 	process.exit( 1 );
 } );
 
-request.end();
-return;
-///////
-//const socket = io( "http://localhost:9011", { // https is supported and can be used.
-const socket = io( "https://genericws.dev.smb.vzc-iot.com:443", {
-	path: "/ws-receiver", // listening path is configurable.
-	query: {
-		tocken: JWT
-	},
-	//transports: [ "websocket" ],
-	//rejectUnauthorized: false // self-signed server cert,
-	transportOptions: {
-		polling: {
-			extraHeaders: {
-				"Cookie": "hello=world"
-			}
-		}
-	}
-} );
-
-// 1) if the server cant validate the token it emit a "error" event an, immediatly after, close the soket.
-// So client must listen to this events
-socket.on( "error", ( error ) => {
-	console.log( "== ERROR ==" );
-	console.dir( error );
-} );
-socket.on( "disconnect", () => {
-	console.log( "== Disconnected from server ==" );
-	process.exit();
-} );
-
-let id = 0;
-
-class Messages {
-	static startSession( socket ) {
-		socket.emit( "startSession", {
-	 		thingType: 20,
-			clientProtocolVersion: "1",
-			id: id.toString(),
-	 		schema: {
-				position: { // name of data schema.
-					dynamic: [ // dynamic field to be populated with value in position messages.
-						"Header.UpdateTime",
-						"Header.SequenceNumber",
-						"GPS.Latitude",
-						"GPS.Longitude",
-						"GPS.FixQuality",
-						"GPS.Speed",
-						"GPS.Heading",
-						"GPS.Altitude",
-						"Radio.GsmSignalLevel",
-						"Radio.GsmCSQ",
-						"PowerSupply.BatVoltage",
-						"Mobile.EventType",
-						"Mobile.CorrelationId"
-					],
-					static: { // static key: value added to all messages.
-						"Mobile.DeviceType": "static-devicetype",
-						"Mobile.DeviceId": "static-deviceid",
-						"Mobile.OS": "WednesdayOS",
-						"Mobile.OSVersion": "007",
-						"Mobile.AppName": "static-FSD",
-						"Mobile.AppVersion": "example"
-					},
-					token: { // Tocken info : standard decoded value to add on messages.
-						"reveal_user_id": "Header.ThingId",
-						"reveal_account_id": "Header.AccountId"
-					}
+// 0.1) Running the client.
+const runClient = ( parsedCoockies = null ) => {
+	const ioClientOptions = {
+		path: "/ws-receiver", // listening path is configurable.
+		query: {
+			token: JWT
+		},
+		//transports: [ "websocket" ],
+		//transports: [ "polling" ],
+		//rejectUnauthorized: false // self-signed server cert,
+	};
+	if( parsedCoockies !== null ) {
+		ioClientOptions.transportOptions = {
+			polling: {
+				extraHeaders: {
+					"Cookie": parsedCoockies
 				}
 			}
-		},
-		( event, payload ) => {
-			if( event === "sessionEstablished" && payload.id === id.toString() ) {
-				// ack received, session estabilished.
-				console.log( "= sessionEstablished =" );
-				// 4) start transmita data (in this case position)
-				setInterval( ( ) => {
-					Messages.sendPosition( socket );
-				}, DATA_DELAY);
-			} else if( event === "sessionFailed" ) {
-				// nack received, session failed.
-				console.log( "sessionFailed: " + payload.error );
-				process.exit();
+		};
+	}
+	const socket = io( "https://genericws.dev.smb.vzc-iot.com:443", ioClientOptions );
+	//const socket = io( "http://localhost:6001", ioClientOptions );
+
+	// 1) if the server cant validate the token it emit a "error" event an, immediatly after, close the soket.
+	// So client must listen to this events
+	socket.on( "connecting", () => {
+		console.log( "==============" );
+		console.log( "= connecting =" );
+		console.log( "==============" );
+	} );
+
+	socket.on( "error", ( error ) => {
+		console.log( "== ERROR ==" );
+		console.dir( error );
+	} );
+	socket.on( "disconnect", () => {
+		console.log( "== Disconnected from server ==" );
+		process.exit();
+	} );
+
+	let id = 0;
+
+	class Messages {
+		static startSession( socket ) {
+			socket.emit( "startSession", {
+		 		thingType: 20,
+				clientProtocolVersion: "1",
+				id: id.toString(),
+		 		schema: {
+					position: { // name of data schema.
+						dynamic: [ // dynamic field to be populated with value in position messages.
+							"Header.UpdateTime",
+							"Header.SequenceNumber",
+							"GPS.Latitude",
+							"GPS.Longitude",
+							"GPS.FixQuality",
+							"GPS.Speed",
+							"GPS.Heading",
+							"GPS.Altitude",
+							"Radio.GsmSignalLevel",
+							"Radio.GsmCSQ",
+							"PowerSupply.BatVoltage",
+							"Mobile.EventType",
+							"Mobile.CorrelationId"
+						],
+						static: { // static key: value added to all messages.
+							"Mobile.DeviceType": "static-devicetype",
+							"Mobile.DeviceId": "static-deviceid",
+							"Mobile.OS": "WednesdayOS",
+							"Mobile.OSVersion": "007",
+							"Mobile.AppName": "static-FSD",
+							"Mobile.AppVersion": "example"
+						},
+						token: { // Tocken info : standard decoded value to add on messages.
+							"reveal_user_id": "Header.ThingId",
+							"reveal_account_id": "Header.AccountId"
+						}
+					}
+				}
+			},
+			( event, payload ) => {
+				if( event === "sessionEstablished" && payload.id === id.toString() ) {
+					// ack received, session estabilished.
+					console.log( "= sessionEstablished =" );
+					// 4) start transmita data (in this case position)
+					setInterval( ( ) => {
+						Messages.sendPosition( socket );
+					}, DATA_DELAY);
+				} else if( event === "sessionFailed" ) {
+					// nack received, session failed.
+					console.log( "sessionFailed: " + payload.error );
+					process.exit();
+				}
 			}
+			);
 		}
-		);
+
+		static sendPosition( socket ) {
+			let stringId = (++id).toString();
+			socket.emit( "position", {
+				id: stringId,
+				data: [
+				(new Date()).toISOString(), //"Header.UpdateTime",
+				parseInt( stringId ), //"Header.SequenceNumber",
+				44.9877331, //"GPS.Latitude",
+				12.0287777, //"GPS.Longitude",
+				100, //"GPS.FixQuality",
+				80, //"GPS.Speed",
+				0.12, //"GPS.Heading",
+				10, //"GPS.Altitude",
+				22, //"Radio.GsmSignalLevel",
+				33, //"Radio.GsmCSQ",
+				12, //"PowerSupply.BatVoltage",
+				15, //"Mobile.EventType",
+				"idontknow" //"Mobile.CorrelationId",
+			] },
+			( event, payload ) => { // 5) check receiving
+				if( event === "ack" && payload.id === stringId ) {
+					console.log( "Ack pkt " + stringId );
+					// packet acked
+				} else if( event === "nack" ) {
+					// packet nacked; retry o discard.
+					console.error( payload.error );
+					process.exit();
+				}
+			} );
+		} 
 	}
 
-	static sendPosition( socket ) {
-		let stringId = (++id).toString();
-		socket.emit( "position", {
-			id: stringId,
-			data: [
-			(new Date()).toISOString(), //"Header.UpdateTime",
-			parseInt( stringId ), //"Header.SequenceNumber",
-			44.9877331, //"GPS.Latitude",
-			12.0287777, //"GPS.Longitude",
-			100, //"GPS.FixQuality",
-			80, //"GPS.Speed",
-			0.12, //"GPS.Heading",
-			10, //"GPS.Altitude",
-			22, //"Radio.GsmSignalLevel",
-			33, //"Radio.GsmCSQ",
-			12, //"PowerSupply.BatVoltage",
-			15, //"Mobile.EventType",
-			"idontknow" //"Mobile.CorrelationId",
-		] },
-		( event, payload ) => { // 5) check receiving
-			if( event === "ack" && payload.id === stringId ) {
-				console.log( "Ack pkt " + stringId );
-				// packet acked
-			} else if( event === "nack" ) {
-				// packet nacked; retry o discard.
-				console.error( payload.error );
-				process.exit();
-			}
-		} );
-	} 
-}
-
-// 2) when the server validate the token the "connect" event is raised.
-socket.on( "connect", () => {
-	console.log( "== Connected to the server ==" );
-	// 3) now client can emit a "startSession" event
-	Messages.startSession( socket );
-} );
+	// 2) when the server validate the token the "connect" event is raised.
+	socket.on( "connect", () => {
+		console.log( "== Connected to the server ==" );
+		// 3) now client can emit a "startSession" event
+		Messages.startSession( socket );
+	} );
+};
