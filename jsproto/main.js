ddf59b7776b0d757fb8c00d254e148a0523fe5c2
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-05-28 17:19:18 +0200

    minchia

---
+++ jsproto/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//const len = 65535;
+const len = 2048;
+const char = "A";
+
+let string = "";
+
+for( let i = 0 ; i < len ; i ++ ) {
+    string += char;
+}
+
+console.log( string );
