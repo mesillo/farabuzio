46ac810cf2522191e1901b5783406de7f4e6acc6
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2020-06-23 09:08:44 +0200

    stocazzolo per due

--- jsproto/sts4/testSocketIO/getToken.js
+++ jsproto/sts4/testSocketIO/getToken.js
@@ -8,7 +8,8 @@ let passwd = "tiger.test";
 let endpoint = "fsdapi.test.us.fm-cloud.com";
 let path = "/mobile/v1/auth/token";
 
-let correlationId = "926181b7-9102-4278-8638-6b40f88c6d7e";
+//let correlationId = "926181b7-9102-4278-8638-6b40f88c6d7e";
+let correlationId = "uuid-a-caso";
 
 let authString = Buffer.from( `${user}:${passwd}` ).toString( "base64" );
 
@@ -20,7 +21,8 @@ let postOptions = {
 	method: "POST",
 	headers: {
 		"Correlation-Id": correlationId,
-		"accept": "*/*",
+		//"accept": "*/*",
+		"accept": "application/json",
 		"Authorization": `Basic ${authString}`
 	}
 };
