ec594c2f20aaa7bee9ac92a6fb428a0dbe185fa2
Alberto Mesin <Alberto.Mesin@fleetmatics.com>
2021-01-05 12:19:42 +0100

    Comments

--- platformIO/vespazio/src/main.cpp
+++ platformIO/vespazio/src/main.cpp
@@ -4,30 +4,11 @@
 #include <rpmCounter.h>
 
 #define PINTONE 8
-#define PINFREQUENCY 12000
+#define PINFREQUENCY 12000 // 16 <-> 200 Hz : 1000 <-> 12000 RPM
 
-/// TIMER ///
-//Timerazio tmz = Timerazio( 1000 );
-//bool toggle( void ) {
-//	static bool status = LOW;
-//	status = ! status;
-//	return status;
-//}
-//void handlerFn( void ) {
-//	digitalWrite( LED_BUILTIN, toggle() );
-//}
-/////////////
-
-// unsigned int prevCount = 0;
-// float prevFreq = -1;
 int prevFreq = -1;
 
 void setup() {
-	/// TIMER ///
-	//pinMode( LED_BUILTIN, OUTPUT );
-	//tmz.setHandler( &handlerFn );
-	//tmz.start();
-	/////////////
 	Serial.begin( 9600 );
 
 	RpmCounter::init();
@@ -37,19 +18,10 @@ void setup() {
 }
 
 void loop() {
-	/// TIMER ///
-	//tmz.loop();
-	/////////////
 	// float freq = RpmCounter::getFrequency();
 	int freq = (int) RpmCounter::getFrequency();
 	if( freq != prevFreq && freq != -1 ) {
 		prevFreq = freq;
 		Serial.println( freq );
 	}
-	/////////////
-	//unsigned int count = RpmCounter::getCount();
-	//if( count != prevCount ) {
-	//	prevCount = count;
-	//	Serial.println( count );
-	//}
 }
