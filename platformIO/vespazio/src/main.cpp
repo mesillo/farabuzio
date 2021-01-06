05f398b7cc4cf3e290d1f8344abda529d8a2ca5c
Alberto Mesin <a.mesin@libero.it>
2021-01-06 15:50:23 +0100

    oled

--- platformIO/vespazio/src/main.cpp
+++ platformIO/vespazio/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 
+#include <SPI.h>
+#include <U8x8lib.h>
+
 //#include <rpmCounter.h>
 #include <rpmCnt.h>
 
 #define PINTONE 8
-#define PINFREQUENCY 200 // 16 <-> 200 Hz : 1000 <-> 12000 RPM (tone() low limit 31Hz)
+#define PINFREQUENCY 1000 // 16 <-> 200 Hz : 1000 <-> 12000 RPM (tone() low limit 31Hz)
 
 int prevFreq = RPMCNT_UNAVAILABLE;
 //int prevRpm = RPMCOUNTER_UNAVAILABLE;
 int prevRpm = RPMCNT_UNAVAILABLE;
 
-void setup() {
-	Serial.begin( 9600 );
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8( U8X8_PIN_NONE );
 
+void setup() {
+	// Serial.begin( 9600 );
+	u8x8.begin();
+	u8x8.setFont(u8x8_font_courR24_3x4_n);
 	// RpmCounter::init();
 	// RpmCounter::enable();
 	RpmCnt::init();
@@ -30,7 +36,9 @@ void loop() {
 	int freq = (int) RpmCnt::getFrequency();
 	if( freq != prevFreq && freq != -1 ) {
 		prevFreq = freq;
-		Serial.println( freq );
+		u8x8.setCursor( 2, 3 );
+		u8x8.print( freq );
+		//Serial.println( freq );
 	}
 	//////////////////////////////////////////
 	// int rpm = RpmCounter::getRPM();
