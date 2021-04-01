5b43c055bc02f0a6d6fce7fb385a152183e33481
Mesillo <mesin.alberto@gmail.com>
2021-04-01 16:02:42 +0200

    Update tcpServer.js

--- jsproto/general/tcpServer.js
+++ jsproto/general/tcpServer.js
@@ -1,10 +1,26 @@
 "use strict";
 
+const PORT = 1620;
+
 const net = require( "net" );
 
 const server = net.createServer( ( socket ) => {
-	socket.write( "ACK" );
+	//socket.write( "ACK" );
 	//socket.pipe( socket );
+	socket.on( "error", ( error ) => {
+		console.error( error );
+	} );
+
+	socket.on( "close", () => {
+		console.info( "Connection Closed" );
+	} );
+
+	socket.on( "data", ( data ) => {
+		console.info( `Received: ${data}` );
+		socket.write( "ACK" );
+	} );
 } );
 
-server.listen( 1620, "127.0.0.1" );
+server.listen( PORT, "127.0.0.1", () => {
+	console.info( `Listen on: ${PORT}.` );
+} );
