f8855034f817d52cc196e0cd88edb62c0c410882
Alberto Mesin <mesin.alberto@gmail.com>
2021-04-02 12:50:39 +0200

    random packets.

---
+++ jsproto/general/packetSender/tcpServer.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const PORT = 1620;
+
+const net = require( "net" );
+
+const server = net.createServer( ( socket ) => {
+	//socket.write( "ACK" );
+	//socket.pipe( socket );
+	socket.on( "error", ( error ) => {
+		console.error( error );
+	} );
+
+	socket.on( "close", () => {
+		//console.info( "Connection Closed" );
+	} );
+
+	socket.on( "data", ( data ) => {
+		//console.info( `Received: ${data}` );
+		console.info( data );
+		socket.write( "ACK" );
+	} );
+} );
+
+server.listen( PORT, "127.0.0.1", () => {
+	console.info( `Listen on: ${PORT}.` );
+} );
