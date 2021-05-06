7272c7b45c187a6fff05f3b46b0135feee71908e
Alberto Mesin <alberto.mesin@visirun.com>
2021-05-06 17:27:33 +0200

    test

---
+++ csproto/testRandom/CoordinateGenerator.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Simulators.Utils
+{
+	static class CoordinateGenerator
+	{
+		//private static readonly double MIN_LATITUDE = 43.69064444988243;
+		//private static readonly double MAX_LATITUDE = 50.58981994995728;
+		//private static readonly double MIN_LONGITUDE = -2.4593354173687616;
+		//private static readonly double MAX_LONGITUDE = 65.1719118769574;
+		private static readonly double MIN_LATITUDE = -90;
+		private static readonly double MAX_LATITUDE = 90;
+		private static readonly double MIN_LONGITUDE = -180;
+		private static readonly double MAX_LONGITUDE = 180;
+		private static readonly Random random = new Random();
+		public static double getLatitude()
+		{
+			return MIN_LATITUDE + ( random.NextDouble() * ( MAX_LATITUDE - MIN_LATITUDE ) );
+		}
+		public static double getLongitude()
+		{
+			return MIN_LONGITUDE + ( random.NextDouble() * ( MAX_LONGITUDE - MIN_LONGITUDE ) );
+		}
+	}
+}
---
+++ csproto/testRandom/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using Simulators.Utils;
+
+namespace testRandom
+{
+	class Program
+	{
+		public const uint samplesNum = 1000000;
+		public const int round = 6;
+		static void Main(string[] args)
+		{
+			Dictionary<double, uint> latitudeMap = new Dictionary<double, uint>();
+			//while( true ) {
+			for( uint i = 0 ; i < samplesNum ; i++ ) {
+				// Console.WriteLine( Math.Round( CoordinateGenerator.getLatitude(), 6 ) );
+				double latitude = Math.Round( CoordinateGenerator.getLatitude(), round );
+				//double latitude = CoordinateGenerator.getLatitude();
+				if( ! latitudeMap.ContainsKey( latitude ) ) {
+					latitudeMap.Add( latitude, 1 );
+				} else {
+					latitudeMap[latitude] += 1;
+				}
+				//Thread.Sleep( 250 ); 
+			}
+			//Console.WriteLine( latitudeMap );
+			uint latCount = 0;
+			uint latMaxTimes = 1;
+			foreach( KeyValuePair<double, uint> latitudeOccurrence in latitudeMap ) {
+				if( latitudeOccurrence.Value > 1 ) {
+					//Console.WriteLine( "{0} -> {1}", latitudeOccurrence.Value, latitudeOccurrence.Key );
+					latCount++;
+					if( latitudeOccurrence.Value > latMaxTimes ) {
+						latMaxTimes = latitudeOccurrence.Value;
+					}
+				}
+			}
+			Console.WriteLine( "Latitude Samples: {0}", samplesNum );
+			Console.WriteLine( "Duplicated values: {0}", latCount );
+			Console.WriteLine( "Max duplicated times: {0}", latMaxTimes );
+		}
+	}
+}
