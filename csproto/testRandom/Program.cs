d94f5ceaae5b1268351bfd56df8219afbc8eb61e
Alberto Mesin <alberto.mesin@visirun.com>
2021-05-07 16:59:09 +0200

    cose orrende

--- csproto/testRandom/Program.cs
+++ csproto/testRandom/Program.cs
@@ -7,19 +7,50 @@ namespace testRandom
 {
 	class Program
 	{
-		public const uint samplesNum = 1000000;
+		public const uint samplesNum = 2147483648;
+		public const uint checkPoint = 100000;
 		public const int round = 6;
 		static void Main(string[] args)
 		{
 			//latitudeTest();
-			allCoordinatesGeneration();
+			//allCoordinatesGeneration( "\t" );
+			//allCoordinatesGeneration( "|" );
+			testFullCoordinates( samplesNum );
 		}
-		static void allCoordinatesGeneration() {
+		static void allCoordinatesGeneration( string separator ) {
 			for( uint i = 0 ; i < samplesNum ; i++ ) {
-				double latitude = Math.Round( CoordinateGenerator.getLatitude(), round );
-				double longitude = Math.Round( CoordinateGenerator.getLongitude(), round );
+				Console.WriteLine( getCoordinate( separator ) );
+			}
+		}
+		static string getCoordinate( string separator ) {
+			double latitude = Math.Round( CoordinateGenerator.getLatitude(), round );
+			double longitude = Math.Round( CoordinateGenerator.getLongitude(), round );
 
-				Console.WriteLine( "{0}\t{1}", latitude, longitude );
+			return string.Format( "{0}{2}{1}", latitude, longitude, separator );
+		}
+		static void testFullCoordinates( uint samplesNum ) {
+			Dictionary<string, uint> map = new Dictionary<string, uint>();
+			for( uint i = 0 ; i < samplesNum ; i++ ) {
+				string coordinateString = getCoordinate( "|" );
+				if( ! map.ContainsKey( coordinateString ) ) {
+					map.Add( coordinateString, 1 );
+				} else {
+					map[coordinateString] += 1;
+				}
+				if( i % checkPoint == 0 ) {
+					Console.WriteLine( " ===== {0} ===== ", i );
+					foreach( KeyValuePair<string, uint> sample in map ) {
+						if( sample.Value > 1 ) {
+							Console.WriteLine( "{0} -> {1}", sample.Value, sample.Key );
+						}
+					}
+				}
+			}
+			Console.WriteLine( " ===== DONE ===== " );
+			foreach( KeyValuePair<string, uint> sample in map ) {
+				if( sample.Value > 1 ) {
+					Console.WriteLine( "{0} -> {1}", sample.Value, sample.Key );
+				}
 			}
 		}
 		static void latitudeTest() {
