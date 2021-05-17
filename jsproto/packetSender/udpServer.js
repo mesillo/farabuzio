68c2478c99fec6cfb98feb099ef11aa1fc21470d
Alberto Mesin <alberto.mesin@visirun.com>
2021-05-17 17:33:56 +0200

    udp packet sendere base...

---
+++ jsproto/packetSender/udpServer.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const udp = require( "dgram" );
+// --------------------creating a udp server --------------------
+
+const PORT = 1234;
+
+// creating a udp server
+const server = udp.createSocket( "udp4" );
+
+// emits when any error occurs
+server.on( "error", ( error ) => {
+	console.error( "=== Error server event ===" );
+	console.error( error );
+	//server.close();
+});
+
+// emits on new datagram msg
+server.on( "message", ( message, infos ) => {
+	//console.log( "Data received from client : " + message.toString() );
+	//console.log( "Received %d bytes from %s:%d", message.length, infos.address, infos.port );
+	console.log( `Received ${message.length} bytes from ${infos.address}.` );
+
+	//sending msg // ACK
+	server.send( "ACK", infos.port, infos.address, ( error ) => {
+		if( error ) {
+			//client.close();
+			console.error( "=== Error on sending ===" );
+			console.error( error );
+		} else {
+			console.log( "ACK sended." );
+		}
+	} );
+} );
+
+//emits when socket is ready and listening for datagram msgs
+server.on( "listening", () => {
+	const address = server.address();
+	const port = address.port;
+	const family = address.family;
+	const ipaddr = address.address;
+	console.log( "Server is listening at port" + port );
+	console.log( "Server ip:" + ipaddr );
+	console.log( "Server is IP4/IP6: " + family );
+} );
+
+//emits after the socket is closed using socket.close();
+server.on( "close", () => {
+  console.log( "Socket is closed!" );
+} );
+
+server.bind( PORT );
+
+//setTimeout( () => {
+//	server.close();
+//}, 8000 );
